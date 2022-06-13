b05f3a42b8067c1b0694390bbc087132a285ccdd
lovxonvy <87499021+triennale@users.noreply.github.com>
2022-06-13 13:09:54 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+import time
+def g(choice):
+    Data = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+    result = ""
+    while choice >= 1:
+        c = random.choice(Data)
+        result = result + c
+        choice=choice-1
+    return result
+print("quanti codici vuoi generare?")
+number = input(">")
+print("")
+n = int(number)
+n = int(number)
+text = input("Type Amazon to get codes -")
+t = text.lower()
+if text == "Amazon":
+    for i in range(n):
+        print(g(4)+"-"+g(6)+"-"+g(5))
+
+else:
+    print("u have typed wrong type like this Amazon")
+
+
+time.sleep(2);
